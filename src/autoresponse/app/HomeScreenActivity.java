68e85b89c3f485ccd1abe2019f31889b06a8f554
Riley Lynch <jrlynchii@gmail.com>
2012-10-19 19:49:16 -0500

    Changed MainActivity into HomeScreenActivity and tweaked resource files appropriately.

--- src/autoresponse/app/HomeScreenActivity.java
+++ src/autoresponse/app/HomeScreenActivity.java
@@ -4,17 +4,17 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class HomeScreenActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home_screen);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+        getMenuInflater().inflate(R.menu.activity_home_screen, menu);
         return true;
     }
 }
