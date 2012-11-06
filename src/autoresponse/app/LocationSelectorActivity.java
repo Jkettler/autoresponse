616e3ef0b7307529b2b0c9e4da2ed370f7ccd693
Riley Lynch <jrlynchii@gmail.com>
2012-11-06 16:54:33 -0600

    Added selection to the listview in LocationSelectorActivity

--- src/autoresponse/app/LocationSelectorActivity.java
+++ src/autoresponse/app/LocationSelectorActivity.java
@@ -5,12 +5,20 @@ import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
 import autoresponse.util.AutoResponseEvent;
 
 public class LocationSelectorActivity extends Activity {
 
-	AutoResponseEvent mEvent;
+	private AutoResponseEvent mEvent;
+	private ListView mEventListView;
+
+	private String selectedLocationName;
+	private boolean locationSelected = false;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,16 +27,27 @@ public class LocationSelectorActivity extends Activity {
         Intent intent = getIntent();
         mEvent = intent.getParcelableExtra(AutoResponseEvent.EVENT_KEY);
         
-    	//TODO Add some kind of list-selector to the UI and assign locationSelected based on that
-    	//TODO pull in location list
-    	//TODO populate list view
+        //TODO pull in location list
+    	//TODO populate list view for real instead of using this fake list
+        String[] items = new String[] {"Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6", "Item 7"};
+        ArrayAdapter<String> adapter =
+          new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, items);
+        
+        mEventListView = (ListView) findViewById(R.id.location_list_view);
+        
+        mEventListView.setAdapter(adapter);
+        
+        mEventListView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> myAdapter, View myView, int myItemInt, long mylng) {
+              selectedLocationName =(String) (mEventListView.getItemAtPosition(myItemInt));
+              locationSelected = true;
+            }                 
+      });
+
     }
     
     public void createResponse(View view){
 
-    	boolean locationSelected = false; 
-    	
-    	
     	if(locationSelected){
     		//TODO Start response selector and pass along whatever data we got from the condition selector
     		//Don't forget to include the location too
